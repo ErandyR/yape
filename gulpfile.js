bec36a7cb34fa17d49e751fd04c19b2c9983753e
ErandyR <rerandy15@gmail.com>
2017-07-19 14:55:22 -0500

    validacion de campos, comenzando segunda seccion

--- gulpfile.js
+++ gulpfile.js
@@ -4,37 +4,44 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 var config = {
-  source: './src/',
-  dist: './public/'
+    source: './src/',
+    dist: './public/'
 };
 var paths = {
-  html: "**/*.html",
-  assets: "assets/",
-  sass: "scss/**/.scss",
-  mainSass: "scss/main.scss",
-  viewHTML1: "view/view.html"
+    html: "**/*.html",
+    assets: "assets/",
+    sass: "scss/**/.scss",
+    mainSass: "scss/main.scss",
+    views: "views/",
+    viewHTML2: "views/view2.html",
+    viewHTML3: "views/view3.html"
 }
 var sources = {
-  assets: config.source + paths.assets,
-  html: config.source + paths.html,
-  sass: paths.assets + paths.sass,
-  rootSass: config.source + paths.assets + paths.mainSass,
-  viewHTML1: config.source + paths.assets + paths.viewHTML1
+    assets: config.source + paths.assets,
+    html: config.source + paths.html,
+    sass: paths.assets + paths.sass,
+    rootSass: config.source + paths.assets + paths.mainSass,
+    viewHTML2: config.source + paths.viewHTML2,
+    viewHTML3: config.source + paths.viewHTML3
 };
-gulp.task('html', function() {
-  gulp.src(sources.html)
-    .pipe(gulp.dest(config.dist));
+gulp.task('html', function () {
+    gulp.src(sources.html)
+        .pipe(gulp.dest(config.dist));
 });
 
-gulp.task('sass', function() {
-  gulp.src(sources.rootSass)
-    .pipe(sass({
-      outputStyle: "compressed"
-    }).on("error", sass.logError))
-    .pipe(gulp.dest('./public/assets/css'));
+gulp.task('sass', function () {
+    gulp.src(sources.rootSass)
+        .pipe(sass({
+            outputStyle: "compressed"
+        }).on("error", sass.logError))
+        .pipe(gulp.dest('./public/assets/css'));
 });
-gulp.task('view', function() {
-  gulp.src(sources.viewHTML1)
-    .pipe(gulp.dest(config.dist + paths.assets + 'views'))
-})
-gulp.task('llamarTarea', ['html', 'sass', 'view']);
+gulp.task('view2', function () {
+    gulp.src(sources.viewHTML2)
+        .pipe(gulp.dest(config.dist + paths.views));
+});
+gulp.task('view3', function () {
+    gulp.src(sources.viewHTML3)
+        .pipe(gulp.dest(config.dist + paths.views));
+});
+gulp.task('llamarTareas', ['html', 'sass', 'view2', 'view3']);
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,34 @@
-$(document).ready(function(){
-      $('.carousel.carousel-slider').carousel({fullWidth: true});
+var url = {
+    url: 'https://localhost:3000/api/registerNumber'
+}
+var cargarPagina = function() {
+    cargarCarrusel();
+    $("#icon_telephone").keyup(telefonoTerminosValidados);
+    $("#filled-in-box").change(telefonoTerminosValidados);
+    $(document).on("click", "#btnContinuar", continuarRegistro);
+}
+
+function cargarCarrusel() {
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
     });
+    $('.carousel').carousel();
+}
+
+function telefonoTerminosValidados() {
+    var $inputNumber = $("#icon_telephone").val();
+    var $inputCheckbox = $("#filled-in-box");
+    var $btnContinuar = $("#btnContinuar");
+    if($inputNumber.length == 10 && $inputCheckbox.prop("checked")){
+        $btnContinuar.removeClass("disabled");
+    }
+    else{
+        $btnContinuar.addClass("disabled");
+    }
+}
+
+function continuarRegistro(){
+    
+}
+
+$(document).ready(cargarPagina);
